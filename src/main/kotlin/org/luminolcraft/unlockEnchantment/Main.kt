78297858264c74cc2cbbcfb3300c33cd26676630
NatJerry <xiaomajunqwq123@outlook.com>
2026-07-06 20:15:04 +0800

    feat: Add config and its management system

--- src/main/kotlin/org/luminolcraft/unlockEnchantment/Main.kt
+++ src/main/kotlin/org/luminolcraft/unlockEnchantment/Main.kt
@@ -2,17 +2,22 @@ package org.luminolcraft.unlockEnchantment
 
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.luminolcraft.unlockEnchantment.config.ConfigManager
 
 
 //TODO 添加配置文件，实现加载插件却不开启附魔活动，以符合维护要求
 class Main : JavaPlugin() {
 
     override fun onEnable() {
+        configManager = ConfigManager(config)
         Bukkit.getPluginManager().registerEvents(AnvilListener(), this)
         logger.info("Loaded")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+    companion object {
+        lateinit var configManager: ConfigManager
     }
 }
---
+++ src/main/kotlin/org/luminolcraft/unlockEnchantment/config/ConfigManager.kt
@@ -0,0 +1,93 @@
+package org.luminolcraft.unlockEnchantment.config
+
+import com.google.common.collect.Lists
+import org.bukkit.configuration.file.FileConfiguration
+import java.io.File
+
+class ConfigManager(config: FileConfiguration) {
+    val config: FileConfiguration = config
+    val configFile: File = File(config.currentPath)
+
+    var isPluginEnabled: Boolean = false
+    var isEnchantmentSimplify:Boolean = false
+    var maximumLevelCost: Int = -1
+    var blacklist: List<String> = Lists.newArrayList()
+
+    fun initConfig() {
+        if (!configFile.exists()) {
+            configFile.mkdirs()
+            configFile.createNewFile()
+            config.load(configFile)
+            if (config.get("enabled") == null) {
+                config.set("enabled", true)
+                config.setComments("enabled", listOf("Set whether to enable the plugin's function"))
+            }
+            if (config.get("simplify-enchantment") == null) {
+                config.set("simplify-enchantment", true)
+                config.setComments("simplify-enchantment", listOf("Set whether to simplify the enchantment"))
+            }
+            if (config.get("maximum-level-cost") == null) {
+                config.set("maximum-level-cost", -1)
+                config.setComments(
+                    "maximum-level-cost",
+                    listOf("Set the value of maximum level cost", "Set -1 to make the cost unlimited")
+                )
+            }
+            if (config.get("blacklist") == null) {
+                config.set("blacklist", listOf(null))
+                config.setComments(
+                    "blacklist", listOf(
+                        "Set which enchantment should be ignored",
+                        "making it didn't be applied in this plugin",
+                        "To check the name of each enchantment,",
+                        "See: https://jd.papermc.io/folia/26.1.2/org/bukkit/enchantments/Enchantment.html",
+                        "Example:",
+                        "blacklist:",
+                        "  - SHARPNESS"
+                    )
+                )
+
+            }
+            if (config.get("special-enchantment-setting") == null) {
+                config.set("special-enchantment-setting", listOf(null))
+                config.setComments(
+                    "special-enchantment-setting", listOf(
+                        "Set which enchantment should be specially adjusted",
+                        "Example:",
+                        "special-enchantment-setting:",
+                        "  SHARPNESS:",
+                        "    maximum-level-cost: 114",
+                        "    maximum-level: 5"
+                    )
+                )
+            }
+            config.save(configFile)
+        }
+    }
+
+    fun loadConfig() {
+        initConfig()
+        isPluginEnabled = config.getBoolean("enabled", true)
+        isEnchantmentSimplify = config.getBoolean("simplify-enchantment", true)
+        maximumLevelCost = config.getInt("maximum-level-cost", -1)
+        blacklist = config.getStringList("blacklist")
+    }
+
+    fun reloadConfig() {
+        initConfig()
+        config.load(configFile)
+        loadConfig()
+    }
+    fun getIsPluginEnabled(): Boolean {
+        return isPluginEnabled
+    }
+    fun getIsEnchantmentSimplify(): Boolean {
+        return isEnchantmentSimplify
+    }
+    fun getMaximumLevelCost(): Int {
+        return maximumLevelCost
+    }
+    fun getBlacklist(): List<String> {
+        return blacklist
+    }
+}
