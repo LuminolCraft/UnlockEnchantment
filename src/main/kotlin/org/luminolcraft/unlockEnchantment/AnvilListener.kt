41470e4e78bb874d4631a5513aa2c446892ba08b
NatJerry <xiaomajunqwq123@outlook.com>
2026-07-06 16:38:03 +0800

    Basically done

--- src/main/kotlin/org/luminolcraft/unlockEnchantment/AnvilListener.kt
+++ src/main/kotlin/org/luminolcraft/unlockEnchantment/AnvilListener.kt
@@ -1,10 +1,12 @@
 package org.luminolcraft.unlockEnchantment
 
 import org.bukkit.Material
+import org.bukkit.Tag
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent
 import org.bukkit.event.inventory.PrepareAnvilEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.EnchantmentStorageMeta
@@ -13,44 +15,127 @@ class AnvilListener : Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onPrepareAnvil(event: PrepareAnvilEvent) {
+//        println("Event Handled")
         val result: ItemStack = event.result ?: return
-        if (event.inventory.firstItem == null || event.inventory.secondItem == null) return
+        val firstItem: ItemStack? = event.inventory.firstItem
+        val secondItem: ItemStack? = event.inventory.secondItem
+//        println("If no more log,bugs.Stage 1")
+        if (firstItem!!.isEmpty == true || event.inventory.secondItem!!.isEmpty) return
+//        println("If no more log,bugs.Stage 2")
+//        val firstItemEnchants: Map<Enchantment?, Int?>?
+//        firstItemEnchants = if (event.inventory.firstItem?.type == Material.ENCHANTED_BOOK
+//            && event.inventory.firstItem!!.itemMeta is EnchantmentStorageMeta
+//        )
+//            (event.inventory.firstItem?.itemMeta as EnchantmentStorageMeta).enchants
+//        else
+//            event.inventory.firstItem?.enchantments
+//        val secondItemEnchants: Map<Enchantment?, Int?>?
+//        secondItemEnchants = if (event.inventory.secondItem?.type == Material.ENCHANTED_BOOK
+//            && event.inventory.secondItem!!.itemMeta is EnchantmentStorageMeta
+//        )
+//            (event.inventory.secondItem?.itemMeta as EnchantmentStorageMeta).enchants
+//        else
+//            event.inventory.secondItem?.enchantments
+
+        //DeepSeek solution
         val firstItemEnchants: Map<Enchantment?, Int?>? =
-            if (event.inventory.firstItem?.getType() == Material.ENCHANTED_BOOK
-                && event.inventory.firstItem?.itemMeta is EnchantmentStorageMeta
-            )
-                event.inventory.firstItem?.itemMeta?.enchants
-            else
-                event.inventory.firstItem?.getEnchantments()
-        val secondItemEnchants: Map<Enchantment?, Int?>? =
-            if (event.inventory.secondItem?.getType() == Material.ENCHANTED_BOOK
-                && event.inventory.secondItem?.itemMeta is EnchantmentStorageMeta
-            )
-                event.inventory.secondItem?.itemMeta?.enchants
-            else
-                event.inventory.secondItem?.getEnchantments()
-
-        if (firstItemEnchants == null || secondItemEnchants == null) return
-
-
-        val ItemEnchants: MutableMap<Enchantment?, Int?> = mutableMapOf<Enchantment?, Int?>()
-        for (s in secondItemEnchants) {
-            if (s.key?.maxLevel == 1) {
-                ItemEnchants.put(s.key, s.key?.startLevel)
-                continue
+            if (firstItem.type == Material.ENCHANTED_BOOK
+                && firstItem.itemMeta is EnchantmentStorageMeta
+            ) {
+                (firstItem.itemMeta as? EnchantmentStorageMeta)?.storedEnchants
+            } else {
+                firstItem.enchantments
             }
 
-            if (firstItemEnchants.containsKey(s.key)) {
-                if (firstItemEnchants[s.key]!! <= 10) {
-                    if (firstItemEnchants[s.key]!! <= s.value!!) {
-                        ItemEnchants.put(s.key, s.value)
-                        continue
-                    }
-                    ItemEnchants.put(s.key, firstItemEnchants[s.key]!!)
+        val secondItemEnchants: Map<Enchantment?, Int?>? =
+            if (secondItem?.type == Material.ENCHANTED_BOOK
+                && secondItem.itemMeta is EnchantmentStorageMeta
+            ) {
+                (secondItem.itemMeta as? EnchantmentStorageMeta)?.storedEnchants
+            } else {
+                secondItem?.enchantments
+            }
+//        println("If no more log,bugs.Stage 3")
+        if (firstItemEnchants == null && secondItemEnchants == null) return
+
+
+        val itemEnchants: MutableMap<Enchantment?, Int?> = mutableMapOf<Enchantment?, Int?>()
+//        println("If no more log,bugs.Stage 4")
+        if (secondItemEnchants != null) {
+            for (s in secondItemEnchants) {
+                //            println("Run into FOR1.")
+                if (s.key?.maxLevel == 1) {
+                    //                println("Run into IF1.")
+                    itemEnchants[s.key] = s.key?.startLevel
                     continue
                 }
 
+                if (firstItemEnchants!!.containsKey(s.key)) {
+                    //                println("Run into IF2.")
+                    if (firstItemEnchants[s.key]!! <= 10 || firstItem!!.type == secondItem!!.type) {
+                        if (firstItemEnchants[s.key]!! < s.value!!) {
+                            itemEnchants[s.key] = s.value
+                            continue
+                        }
+                        if (firstItemEnchants[s.key]!! == s.value!!) {
+                            itemEnchants[s.key] = s.value!! + 1
+                            continue
+                        }
+                        itemEnchants[s.key] = firstItemEnchants[s.key]!!
+                        continue
+                    }
+                    //装备+书
+                    if (
+                        (Tag.ITEMS_PICKAXES.isTagged(firstItem.type) ||
+                                Tag.ITEMS_AXES.isTagged(firstItem.type) ||
+                                Tag.ITEMS_SHOVELS.isTagged(firstItem.type) ||
+                                Tag.ITEMS_SWORDS.isTagged(firstItem.type) ||
+                                Tag.ITEMS_HOES.isTagged(firstItem.type) ||
+                                Tag.ITEMS_CHEST_ARMOR.isTagged(firstItem.type) ||
+                                Tag.ITEMS_FOOT_ARMOR.isTagged(firstItem.type) ||
+                                Tag.ITEMS_LEG_ARMOR.isTagged(firstItem.type) ||
+                                Tag.ITEMS_HEAD_ARMOR.isTagged(firstItem.type)) && secondItem?.type == Material.ENCHANTED_BOOK
+                    ) {
+                        if (firstItemEnchants[s.key]!! < s.value!!) {
+                            itemEnchants[s.key] = s.value
+                            continue
+                        }
+                        if (truncateOnes(firstItemEnchants[s.key]!!) == truncateOnes(secondItemEnchants[s.key]!!)) {
+//                            println("firstItemEnchants[${s.key}] is truncated,value is ${truncateOnes(firstItemEnchants[s.key]!!)},secondItemEnchants[${s.key}] is truncated,value is ${truncateOnes(secondItemEnchants[s.key]!!)}")
+                            itemEnchants[s.key] = firstItemEnchants[s.key]!! + 1
+                            continue
+                        }
+                        itemEnchants[s.key] = firstItemEnchants[s.key]!!
+                        continue
+
+                    }
+                }
             }
+            //补齐第二个物品内存在但第一个物品不存在的附魔
+            for (s in secondItemEnchants) {
+                if (itemEnchants[s.key] != null) {
+                    continue
+                }
+                itemEnchants[s.key] = s.value
+            }
+        }
+        //TODO Add configuration to adjust this value
+        if(event.view.repairCost >= 717){
+            event.view.repairCost = 717
+        }
+
+        for (e in itemEnchants) {
+            e.key?.let {
+                result.addUnsafeEnchantment(it, e.value!!)
+            }
+        }
+//        println("All functions passed.")
+
+    }
+
+    companion object {
+        fun truncateOnes(number: Int): Int {
+            return number / 10 * 10
         }
     }
 
--- src/main/kotlin/org/luminolcraft/unlockEnchantment/Main.kt
+++ src/main/kotlin/org/luminolcraft/unlockEnchantment/Main.kt
@@ -3,10 +3,13 @@ package org.luminolcraft.unlockEnchantment
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
+
+//TODO 添加配置文件，实现加载插件却不开启附魔活动，以符合维护要求
 class Main : JavaPlugin() {
 
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(AnvilListener(), this)
+        logger.info("Loaded")
     }
 
     override fun onDisable() {
